a35b500725624fefedead65fd0c5cd28c307ed3a
George Dumitrescu <george@dumitres.co>
2020-12-02 10:18:41 +0200

    add support for optional {param?}

--- src/assets/js/routeFunction.js
+++ src/assets/js/routeFunction.js
@@ -4,23 +4,28 @@ const route = (routeName, params = [], absolute = true) => {
 
   let uri = _route.uri;
 
-  const matches = uri.match(/{[\w]+}/g) || [];
-  const requiredParametersCount = matches.length;
+  const matches = uri.match(/{[\w]+\??}/g) || [];
+  const optionals = uri.match(/{[\w]+\?}/g) || [];
+
+  const requiredParametersCount = matches.length - optionals.length;
 
   if (params instanceof Array) {
     if (params.length < requiredParametersCount) throw "Missing parameters";
 
     for (let i = 0; i < requiredParametersCount; i++)
-      uri = uri.replace(/{[\w]+}/, params.shift());
+      uri = uri.replace(/{[\w]+\??}/, params.shift());
 
     for (let i = 0; i < params.length; i++)
       uri += (i ? "&" : "?") + params[i] + "=" + params[i];
   } else if (params instanceof Object) {
     let extraParams = matches.reduce((ac, match) => {
       let key = match.substring(1, match.length - 1);
-      if (params.hasOwnProperty(key)) {
-        uri = uri.replace(new RegExp(match, "g"), params[key]);
-        delete ac[key];
+      let isOptional = key.endsWith("?");
+      if (params.hasOwnProperty(key.replace("?", ""))) {
+        uri = uri.replace(new RegExp(match.replace("?", "\\?"), "g"), params[key.replace("?", "")]);
+        delete ac[key.replace("?", "")];
+      } else if (isOptional) {
+          uri = uri.replace("/" + new RegExp(match, "g"), "");
       }
       return ac;
     }, params);
@@ -28,6 +33,12 @@ const route = (routeName, params = [], absolute = true) => {
     Object.keys(extraParams).forEach((key, i) => {
       uri += (i ? "&" : "?") + key + "=" + extraParams[key];
     });
+  }
+
+  if (optionals.length > 0) {
+    for (let i in optionals) {
+      uri = uri.replace("/" + optionals[i], "");
+    }
   }
 
   if (uri.includes("}")) throw "Missing parameters";
