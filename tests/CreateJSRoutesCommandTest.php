d34e5cd6332e0147127dafefaa12a4d6e91a007d
Halí V. <halivelazquez@outlook.com>
2023-09-30 19:12:15 -0600

    feat: update dependencies
    update phpunit

--- tests/CreateJSRoutesCommandTest.php
+++ tests/CreateJSRoutesCommandTest.php
@@ -2,77 +2,75 @@
 
 namespace Halivert\JSRoutes\Tests;
 
-use Halivert\JSRoutes\Console\CreateJSRoutesCommand;
 use Illuminate\Routing\Router;
 use Orchestra\Testbench\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 class CreateJSRoutesCommandTest extends TestCase
 {
-	protected function getPackageProviders($app)
-	{
-		return [
-			'Halivert\JSRoutes\Tests\Stubs\Providers\JSRoutesServiceProviderTest'
-		];
-	}
+    protected function getPackageProviders($app): array
+    {
+        return [
+            'Halivert\JSRoutes\Tests\Stubs\Providers\JSRoutesServiceTestProvider'
+        ];
+    }
 
-	protected function getEnvironmentSetUp($app)
-	{
-		$app['router']->get('get', ['as' => 'get', 'uses' => function () {
-			return 'hello world';
-		}]);
+    protected function getEnvironmentSetUp($app): void
+    {
+        $app['router']->get('get', ['as' => 'get', 'uses' => function () {
+            return 'hello world';
+        }]);
 
-		$app['router']->get('get/{id}', ['as' => 'hi', 'uses' => function () {
-			return 'hello world';
-		}]);
+        $app['router']->get('get/{id}', ['as' => 'hi', 'uses' => function () {
+            return 'hello world';
+        }]);
 
-		$app['router']->post('post', function () {
-			return 'goodbye world';
-		})->name('bye');
+        $app['router']->post('post', function () {
+            return 'goodbye world';
+        })->name('bye');
 
-		$app['router']->group(['prefix' => 'boss'], function (Router $router) {
-			$router->put('put', ['as' => 'boss.hi', 'uses' => function () {
-				return 'hello boss';
-			}]);
+        $app['router']->group(['prefix' => 'boss'], function (Router $router) {
+            $router->put('put', ['as' => 'boss.hi', 'uses' => function () {
+                return 'hello boss';
+            }]);
 
-			$router->delete('delete', function () {
-				return 'goodbye boss';
-			})->name('boss.bye');
-		});
+            $router->delete('delete', function () {
+                return 'goodbye boss';
+            })->name('boss.bye');
+        });
 
 
-		$app['router']->patch('patch', function () {
-			return 'goodbye world';
-		})->name('patch');
+        $app['router']->patch('patch', function () {
+            return 'goodbye world';
+        })->name('patch');
 
-		$app['router']->options('options', function () {
-			return 'goodbye world';
-		})->name('options');
+        $app['router']->options('options', function () {
+            return 'goodbye world';
+        })->name('options');
 
-		$app['router']->get('bruh', function () {
-			return 'goodbye world';
-		})->name('telescope');
+        $app['router']->get('bruh', function () {
+            return 'goodbye world';
+        })->name('telescope');
 
-		$app['router']->get('multi/{id}/param/{id2}/url/{id}', function () {
-			return 'goodbye world';
-		})->name('multiparam');
+        $app['router']->get('multi/{id}/param/{id2}/url/{id}', function () {
+            return 'goodbye world';
+        })->name('multiparam');
 
-		$app['config']->set('app.jsroutes.path', './');
-	}
+        $app['config']->set('app.jsroutes.path', './');
+    }
 
-	/**
-	 * @test
-	 */
-	public function it_creates_file()
-	{
-		if (file_exists(realpath('./routes.js'))) {
-			$this->artisan('route:tojs')->expectsQuestion(
-				"The [routes.js] file already exists. Do you want to replace it?",
-				"yes"
-			)->expectsOutput('routes.js created')->assertExitCode(0);
-		} else {
-			$this->artisan('route:tojs')
-				->expectsOutput('routes.js created')
-				->assertExitCode(0);
-		}
-	}
+    #[Test]
+    public function it_creates_file()
+    {
+        if (file_exists(realpath('./routes.js'))) {
+            $this->artisan('route:tojs')->expectsQuestion(
+                "The [routes.js] file already exists. Do you want to replace it?",
+                "yes"
+            )->expectsOutput('routes.js created')->assertExitCode(0);
+        } else {
+            $this->artisan('route:tojs')
+                ->expectsOutput('routes.js created')
+                ->assertExitCode(0);
+        }
+    }
 }
--- tests/Stubs/Providers/JSRoutesServiceTestProvider.php
+++ tests/Stubs/Providers/JSRoutesServiceTestProvider.php
@@ -5,13 +5,13 @@ namespace Halivert\JSRoutes\Tests\Stubs\Providers;
 use Halivert\JSRoutes\JSRoutesServiceProvider;
 use Illuminate\Support\AggregateServiceProvider;
 
-class JSRoutesServiceProviderTest extends AggregateServiceProvider
+class JSRoutesServiceTestProvider extends AggregateServiceProvider
 {
     protected $providers = [
         JSRoutesServiceProvider::class,
     ];
 
-    public function register()
+    public function register(): void
     {
         parent::register();
 
